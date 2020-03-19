7e9455c6b71141daac8ba21afce808a226bdc092
nicktfranklin <nthompsonsonfranklin@gmail.com>
2020-03-19 15:07:32 -0400

    add multiprocessing for memory management

--- core/utils.py
+++ core/utils.py
@@ -1,4 +1,9 @@
+import os
+import sys
+import traceback
 import numpy as np
+from functools import wraps
+from multiprocessing import Process, Queue
 
 
 def unroll_data(x, t=1):
@@ -85,3 +90,47 @@ def delete_object_attributes(myobj):
         attr = [k for k in myobj.__dict__.keys()][0]
         myobj.__delattr__(attr)
     
+
+def processify(func):
+    '''Decorator to run a function as a process.
+    Be sure that every argument and the return value
+    is *pickable*.
+    The created process is joined, so the code does not
+    run in parallel.
+
+    Credit: I took this function from Marc Schlaich's github:
+    https://gist.github.com/schlamar/2311116
+    '''
+
+    def process_func(q, *args, **kwargs):
+        try:
+            ret = func(*args, **kwargs)
+        except Exception:
+            ex_type, ex_value, tb = sys.exc_info()
+            error = ex_type, ex_value, ''.join(traceback.format_tb(tb))
+            ret = None
+        else:
+            error = None
+
+        q.put((ret, error))
+
+    # register original function with different name
+    # in sys.modules so it is pickable
+    process_func.__name__ = func.__name__ + 'processify_func'
+    setattr(sys.modules[__name__], process_func.__name__, process_func)
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        q = Queue()
+        p = Process(target=process_func, args=[q] + list(args), kwargs=kwargs)
+        p.start()
+        ret, error = q.get()
+        p.join()
+
+        if error:
+            ex_type, ex_value, tb_str = error
+            message = '%s (in subprocess)\n%s' % (ex_value.message, tb_str)
+            raise ex_type(message)
+
+        return ret
+    return wrapper
